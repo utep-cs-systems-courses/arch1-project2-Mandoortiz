64dea3677261331937e7379b520e656aa63bae4c
Armando Ortiz <mandoortiz6209@gmail.com>
2020-04-15 09:31:34 -0600

    updated assembly files

--- project/blinkyBuzzy/assembly.s
+++ project/blinkyBuzzy/assembly.s
@@ -1,47 +1,48 @@
-	.arch msp430g2553
-	
-	.data			; lightshow_state is static
-lightshow_state:
-	.word 0
+	.arch mps430g2553
 
-	.text			;
+	.data			;st is static variable
+st:
+	.word 1
+	.align 2
+
+	.text			
 jt:
-	.word option0		; jt[0]
-	.word option1		; jt[1]
-	.word option2		; jt[2]
-	.word option3		; jt[3]
-	.word default		; jt[4]
+	.word default		;jt[0]
+	.word option1		;jt[1]
+	.word option2		;jt[2]
+	.word option3		;jt[3]
+	.word option4		;jt[4]
 
+	.text
 	.global assembly_lightshow
 assembly_lightshow:
-	cmp #4, &lightshow_state; lightshow_state-4
-	jnc default		; doesn't borrow if lightshow_state > 3
+	cmp #5, &st		;st-5 doesn't is s > 5
+	jc default		;jump if does borrow
 
-	mov &lightshow_state, r12;
-	add r12, r12		;
-	mov jt(r12), r0		;
-option0:
-	mov #0, &red_on		; red_on = 0
-	mov #0, &green_on	; green_on = 0
-	add #1, &lightshow_state; lightshow_state++
+	mov &st, r12
+	add r12, r12		;r12 = 2*st
+	mov jt(r12), r0		;jmp jt[st]
+default:
 	jmp end			; break
 option1:
-	mov #1, &red_on		; red_on = 1
-	mov #0, &green_on	; green_on = 0
-	add #1, &lightshow_state; lightshow_state++
+	mov.b #0, &red_on	; red_on = 0
+	mov.b #0, &green_on	; green_on = 0
+	mov #2, &st		; state = 2
 	jmp end			; break
 option2:
-	mov #0, &red_on		; red_on = 0
-	mov #1, &green_on	; green_on = 1;
-	add #1, &lightshow_state; lightshow_state++
+	mov.b #1, &red_on	; red_on = 1
+	mov.b #0, &green_on	; green_on = 0
+	mov #3, &st		; state = 3
 	jmp end			; break
 option3:
-	mov #1, &red_on		; red_on = 1
-	mov #1, &green_on	; green_on = 1
-	mov #0, &lightshow_state; lightshow_state = 0
+	mov.b #0, &red_on	; red_on = 0
+	mov.b #1, &green_on	; green_on = 1
+	mov #4, &st		; state = 4
 	jmp end			; break
-default:
-	mov #0, &lightshow_state; lightshow_state = 0
+option4:
+	mov.b #1, &red_on	; red_on = 1
+	mov.b #1, &green_on	; green_on = 1
+	mov #1, &st		; state = 1
 	jmp end			; break
-	
-end:	pop r0			; return
+end:
+	pop r0
